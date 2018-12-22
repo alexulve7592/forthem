32142c49e70c3212db038d426a39f9f3d379bd81
alexulve7592 <43419473+alexulve7592@users.noreply.github.com>
2018-12-22 13:36:36 -0600

    Add files via upload

---
+++ finalproject/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    $(".one").mouseenter(function(){
+                    $(".overlay_one").fadeIn();
+                         });
+    $(".one").mouseleave(function(){
+                    $(".overlay_one").fadeOut();
+                         });
+
+    $(".two").mouseenter(function(){
+                    $(".overlay_two").fadeIn();
+                         });
+    $(".two").mouseleave(function(){
+                    $(".overlay_two").fadeOut();
+                         });
+
+    $(".three").mouseenter(function(){
+                    $(".overlay_three").fadeIn();
+                         });
+    $(".three").mouseleave(function(){
+                    $(".overlay_three").fadeOut();
+                         });
+    
+    $(".four").mouseenter(function(){
+                    $(".overlay_four").fadeIn();
+                         });
+    $(".four").mouseleave(function(){
+                    $(".overlay_four").fadeOut();
+                         });
+    
+    $(".five").mouseenter(function(){
+                    $(".overlay_five").fadeIn();
+                         });
+    $(".five").mouseleave(function(){
+                    $(".overlay_five").fadeOut();
+                         });
+    
+    $(".six").mouseenter(function(){
+                    $(".overlay_six").fadeIn();
+                         });
+    $(".six").mouseleave(function(){
+                    $(".overlay_six").fadeOut();
+                         });
+    
+    $(".seven").mouseenter(function(){
+                    $(".overlay_seven").fadeIn();
+                         });
+    $(".seven").mouseleave(function(){
+                    $(".overlay_seven").fadeOut();
+                         });
+    
+    $(".eight").mouseenter(function(){
+                    $(".overlay_eight").fadeIn();
+                         });
+    $(".eight").mouseleave(function(){
+                    $(".overlay_eight").fadeOut();
+                         });
+    
+    $(".nine").mouseenter(function(){
+                    $(".overlay_nine").fadeIn();
+                         });
+    $(".nine").mouseleave(function(){
+                    $(".overlay_nine").fadeOut();
+                         });
+    
+    $(".ten").mouseenter(function(){
+                    $(".overlay_ten").fadeIn();
+                         });
+    $(".ten").mouseleave(function(){
+                    $(".overlay_ten").fadeOut();
+                         });
+    
+    $(".eleven").mouseenter(function(){
+                    $(".overlay_eleven").fadeIn();
+                         });
+    $(".eleven").mouseleave(function(){
+                    $(".overlay_eleven").fadeOut();
+                         });
+    
+    $(".twelve").mouseenter(function(){
+                    $(".overlay_twelve").fadeIn();
+                         });
+    $(".twelve").mouseleave(function(){
+                    $(".overlay_twelve").fadeOut();
+                         });
+  
+});
